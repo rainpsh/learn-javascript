3e37542226a79249033b4ce8cedd4e91c287f17b
rainpsh <rainpsh@naver.com>
2025-09-16 10:01:38 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// 상수(변할수 없는 값)
+const username = "박성혜" 
+console.log("🚀 ~ username:", username)
+
+// 변수(변할수 있는 값)
+let usermbti = "ENTJ" 
+console.log("🚀 ~ usermbti:", usermbti)
+
+usermbti = "ISTJ" //수정
+console.log("🚀 ~ 변경된 usermbti:", usermbti)
+
+// 상수는 변경 불가능
+// username = "후츠릿"
+// console.log("🚀 ~ 변경된 username:", username)
